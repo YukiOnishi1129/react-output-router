5195a24a09ca45e635e81e8839c9533f3b4ae9d4
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-09-27 22:18:30 +0900

    add: main route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { TodoProvider } from "./contexts/TodoContext";
 import { TodoPage } from "./pages/todo";
+import { NAVIGATION_LIST } from "./constants/navigations";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: NAVIGATION_LIST.TOP,
+    element: <TodoPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TodoPage />
+    <TodoProvider>
+      <RouterProvider router={router} />
+    </TodoProvider>
   </React.StrictMode>
 );
