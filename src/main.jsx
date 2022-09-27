2530b6dad6436c5c99d79ba1aed58edd53b23bff
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-09-27 22:31:42 +0900

    add: temporary page

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { TodoProvider } from "./contexts/TodoContext";
-import { TodoPage } from "./pages/todo";
 import { NAVIGATION_LIST } from "./constants/navigations";
+import { TodoPage } from "./pages/todo";
+import { TodoDetailPage } from "./pages/detail.jsx";
 import "./index.css";
+import { TodoCreatePage } from "./pages/create";
+import { TodoEditPage } from "./pages/edit";
 
 const router = createBrowserRouter([
   {
     path: NAVIGATION_LIST.TOP,
     element: <TodoPage />,
+  },
+  {
+    path: NAVIGATION_LIST.DETAIL,
+    element: <TodoDetailPage />,
+  },
+  {
+    path: NAVIGATION_LIST.CREATE,
+    element: <TodoCreatePage />,
+  },
+  {
+    path: NAVIGATION_LIST.EDIT,
+    element: <TodoEditPage />,
   },
 ]);
 
