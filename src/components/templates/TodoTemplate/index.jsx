7403ada459b0217584de0f6606da5c8d93b701a0
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-09-28 23:34:45 +0900

    add: useTodoTemplate

--- src/components/templates/TodoTemplate/index.jsx
+++ src/components/templates/TodoTemplate/index.jsx
@@ -3,11 +3,11 @@
  *
  * @package components
  */
-import { useMemo, useState } from "react";
 import { BaseLayout } from "../../organisms/BaseLayout";
 import { InputForm } from "../../atoms/InputForm";
 import { TodoList } from "../../organisms/TodoLlist";
-import { useTodoContext } from "../../../contexts/TodoContext.jsx";
+import { useTodoContext } from "../../../contexts/TodoContext";
+import { useTodoTemplate } from "./useTodoTemplate";
 import styles from "./styles.module.css";
 
 /**
@@ -19,26 +19,8 @@ export const TodoTemplate = () => {
   // コンテキストから状態とロジックを呼び出してコンポーネントにあてがう
   const { originTodoList, deleteTodo } = useTodoContext();
 
-  /* 検索キーワード */
-  const [searchKeyword, setSearchKeyword] = useState("");
-  /* 表示用TodoList */
-  const showTodoList = useMemo(() => {
-    const regexp = new RegExp("^" + searchKeyword, "i");
-    return originTodoList.filter((todo) => {
-      // 検索キーワードに部分一致したTodoだけを一覧表示する
-      return todo.title.match(regexp);
-    });
-    // useMemoの第二引数([originTodoList, searchKeyword])に依存して処理が実行される
-    // originTodoListとsearchKeywordの値が変更される度にfilterの検索処理が実行
-    // ただし結果が前回と同じならキャッシュを返却し処理は実行されない(無駄な処理を省いている)
-    // 詳しくはuseMemoを調べてください。
-  }, [originTodoList, searchKeyword]);
-
-  /**
-   * 検索キーワード更新処理
-   * @param {*} e
-   */
-  const handleChangeSearchKeyword = (e) => setSearchKeyword(e.target.value);
+  const [{ searchKeyword, showTodoList }, { handleChangeSearchKeyword }] =
+    useTodoTemplate({ originTodoList });
 
   return (
     <BaseLayout title={"TodoList"}>
