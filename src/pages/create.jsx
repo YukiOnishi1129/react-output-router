20fdb51b651e5884146b82e68d7b29c101c86e7f
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-09-28 22:12:31 +0900

    add: create template

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -3,5 +3,11 @@
  *
  * @package pages
  */
+import { TodoCreateTemplate } from "../components/templates/TodoCreateTemplate/index.jsx";
 
-export const TodoCreatePage = () => <div>作成</div>;
+/**
+ * TodoCreatePage
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export const TodoCreatePage = () => <TodoCreateTemplate />;
