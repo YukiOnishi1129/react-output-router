fdac75021cd2f345e4baad4ae092ad7b953f147c
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-09-28 22:12:34 +0900

    add: create template

---
+++ src/components/atoms/TextArea/index.jsx
@@ -0,0 +1,16 @@
+/**
+ * TextArea
+ *
+ * @package components
+ */
+
+import styles from "./styles.module.css";
+
+export const TextArea = ({ value, placeholder, onChange }) => (
+  <textarea
+    className={styles.text}
+    value={value}
+    placeholder={placeholder}
+    onChange={onChange}
+  />
+);
