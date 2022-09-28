45abe5252fd0925b87117ac8768d0690eb9e4f18
yukionishi1129 <yuki.onishi1129@gmail.com>
2022-09-29 00:15:48 +0900

    add: TodoDetailTemplate

--- src/components/atoms/TextArea/index.jsx
+++ src/components/atoms/TextArea/index.jsx
@@ -3,11 +3,25 @@
  *
  * @package components
  */
-
 import styles from "./styles.module.css";
 
-export const TextArea = ({ value, placeholder, onChange }) => (
+/**
+ * TextArea
+ * @param disabled
+ * @param value
+ * @param placeholder
+ * @param onChange
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export const TextArea = ({
+  disabled = false,
+  value,
+  placeholder,
+  onChange,
+}) => (
   <textarea
+    disabled={disabled}
     className={styles.text}
     value={value}
     placeholder={placeholder}
--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -3,5 +3,11 @@
  *
  * @package pages
  */
+import { TodoDetailTemplate } from "../components/templates/TodoDetailTemplate/index";
 
-export const TodoDetailPage = () => <div>詳細</div>;
+/**
+ * TodoDetailPage
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export const TodoDetailPage = () => <TodoDetailTemplate />;
